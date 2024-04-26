8caf7da8c88d887a0f99ae8acb3616c851b64d96
tim_donin <tim.donin.dev@gmail.com>
2024-04-26 20:01:35 +0300

    Optimization

--- app.py
+++ app.py
@@ -4,15 +4,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  
+  return render_template('index.html', title='Main Page', dropdown_items=dropdown_items)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')  
+    return render_template('about.html', title='About', dropdown_items=dropdown_items)  
 
 @app.route('/authorise')
 def authorisation():
-    return render_template('authorisation-page.html')
+    return render_template('authorisation-page.html', title='Enterance', dropdown_items=dropdown_items)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  dropdown_items = [
+    {
+      'name': 'Chairs',
+      'items': [
+        {'name': 'Soft Chairs'},
+        {'name': 'Lether Chairs'},
+        {'name': 'Plastic Chairs'}, 
+        {'name': 'Wooden Chairs'},
+        {'name': 'Metal Chairs'},
+        {'name': 'Kitchen Dining Chairs'}
+        ]
+      },
+    {
+      'name': 'Category 2',
+      'items': [
+        {'name': 'Item 4'},
+        {'name': 'Item 5'},
+        {'name': 'Item 6'}
+        ]
+      },
+    {
+      'name': 'Tables',
+      'items': [
+        {'name': 'Kitchen Tables'},
+        {'name': 'Dining Tables'},
+        {'name': 'Glass Tables'}
+      ]
+    }
+    ]
+  app.run(debug=True)
