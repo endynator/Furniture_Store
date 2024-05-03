e3cf0a10ac0b1d1cddad17efcc9aca19887e61d7
tim_donin <tim.donin.dev@gmail.com>
2024-05-03 19:57:33 +0300

    Finished Navbar

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  
-  return render_template('index.html', title='Main Page', dropdown_items=dropdown_items)
+    return render_template('index.html', title='Main Page', dropdown_items=dropdown_items)
 
 @app.route('/about')
 def about():
@@ -15,34 +14,40 @@ def about():
 def authorisation():
     return render_template('authorisation-page.html', title='Enterance', dropdown_items=dropdown_items)
 
+@app.route('/Item/<string:category>/<int:id>')
+def getItem(category: str, id: int):
+    item_name = dropdown_items[category]['items'][id - 1]['name']
+    title = f'{item_name}'
+    return render_template('index.html', title=title, dropdown_items=dropdown_items)
+
 if __name__ == '__main__':
-  dropdown_items = [
-    {
-      'name': 'Chairs',
-      'items': [
-        {'name': 'Soft Chairs'},
-        {'name': 'Lether Chairs'},
-        {'name': 'Plastic Chairs'}, 
-        {'name': 'Wooden Chairs'},
-        {'name': 'Metal Chairs'},
-        {'name': 'Kitchen Dining Chairs'}
+  dropdown_items = {
+    'Chairs': {
+        'items': [
+            {'name': 'Soft Chairs'},
+            {'name': 'Lether Chairs'},
+            {'name': 'Plastic Chairs'}, 
+            {'name': 'Wooden Chairs'},
+            {'name': 'Metal Chairs'},
+            {'name': 'Kitchen Dining Chairs'}
         ]
-      },
-    {
-      'name': 'Category 2',
-      'items': [
-        {'name': 'Item 4'},
-        {'name': 'Item 5'},
-        {'name': 'Item 6'}
+    },
+    'Category 2': {
+        'items': [
+            {'name': 'Item 4'},
+            {'name': 'Item 5'},
+            {'name': 'Item 6'}
         ]
-      },
-    {
-      'name': 'Tables',
-      'items': [
-        {'name': 'Kitchen Tables'},
-        {'name': 'Dining Tables'},
-        {'name': 'Glass Tables'}
-      ]
+    },
+    'Tables': {
+        'items': [
+            {'name': 'Kitchen Tables'},
+            {'name': 'Dining Tables'},
+            {'name': 'Glass Tables'}
+        ]
     }
-    ]
+}
+  for category, data in dropdown_items.items():
+    for i, item in enumerate(data['items'], start=1):
+        item['id'] = i
   app.run(debug=True)
